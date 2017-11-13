826fafb61740ae06929be1c975c0edc5984a70b0
UtkarshGupta-CS <utgupta1997@gmail.com>
2017-11-13 23:23:46 +0530

    set the constant values

--- src/constants.h
+++ src/constants.h
@@ -3,7 +3,16 @@
 // Buffer to store the respons message
 #define BUFFER_SIZE 638
 
-// Number of threads to be created
-#define NUM_THREADS 5
+// default number of threads to be created
+#define NUM_THREADS 2
+
+// default port number
+#define PORT_NO 80
+
+// default number of of request to be made to the server
+#define NO_OF_REQUESTS 10
+
+// default duration time for the benchmarking will be done
+#define DURATION 10
 
 #endif // !CONSTANTS_H
